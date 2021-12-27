c1742fd7fe27f4bdf2dd6f0919f6285af7fa0529
vorvit <94832434+vorvit@users.noreply.github.com>
2021-12-27 18:59:07 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WPF18
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void spasibo_Click(object sender, RoutedEventArgs e)
+        {
+            player.Pause();
+            player.Volume = 0;
+            PromptBuilder prompt = new PromptBuilder();
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+            prompt.AppendText("Екатерина Сергеевна,");
+            prompt.AppendText("спасибо, ", PromptEmphasis.Strong);
+            prompt.AppendText("за науку!");
+            synthesizer.Speak(prompt);
+            player.Play();
+            player.Volume = 1;
+        }
+    }
+}
